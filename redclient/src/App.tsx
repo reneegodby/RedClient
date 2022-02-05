3b821697ae576dd1fbc4264771eb2fc56e06dbb0
reneegodby <57304584+reneegodby@users.noreply.github.com>
2022-02-05 12:04:48 -0500

    first commit

---
+++ redclient/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FunctionComponent = () => {
+  return (
+    <div className="App">
+      <div className='verticalCenter'>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
