f8bf10c0e058a0a0b33cc3371feb60cf41219b25
reneegodby <57304584+reneegodby@users.noreply.github.com>
2022-02-12 11:02:20 -0500

    fixed url

--- src/helpers/environment.tsx
+++ src/helpers/environment.tsx
@@ -4,8 +4,8 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:5001';
         break;
-    case 'rmg-bakerhelper.herokuapp.com':
-        APIURL = 'https://rg-bakerhelper.herokuapp.com'
+    case 'rmg-bakerhelper.herokuapp.com/':
+        APIURL = 'https://rg-bakerhelper.herokuapp.com/' 
 }
 
 export default APIURL;
